ab4398867b43dfe70a229983732dfc6f0a0af0fc
iamnotrodger <rodger.retanal24@gmail.com>
2023-06-27 08:38:50 -0400

    [feature] `SpaceWarp` and `Hero` section animations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,28 +1,72 @@
-import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
 import Button from '../elements/Button';
 import Input from '../elements/Input';
 import Logo from '../elements/Logo';
-import Section from '../elements/Section';
 import SectionTitle from '../elements/SectionTitle';
+import SpaceWarp from './SpaceWarp';
+import Section from '../elements/Section';
 
 const Hero = () => {
+	const [show, setShow] = useState(false);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setShow(true);
+		}, 2500);
+		return () => clearTimeout(timer);
+	}, []);
+
 	return (
-		<Section className='min-h-screen bg-indigo-950 flex flex-col'>
-			<Logo className='mb-20' />
-			<div className='flex flex-col gap-10'>
-				<div className='flex flex-col gap-4 text-center'>
-					<SectionTitle>Master the Cosmos with Galactic Shogun</SectionTitle>
-					<p className='text-sm text-tertiary-light'>
-						Sign up now to receive exclusive updates and be the first to conquer
-						the cosmos!
-					</p>
-				</div>
-				<div className='flex flex-col gap-2'>
-					<Input placeholder='Enter your email' />
-					<Button>SUBSCRIBE</Button>
-				</div>
-			</div>
-		</Section>
+		<>
+			<Canvas
+				className='bg-black min-h-screen'
+				style={{ position: 'absolute' }}
+				camera={{
+					fov: 100,
+					near: 0.1,
+					far: 200,
+				}}
+			>
+				<SpaceWarp />
+			</Canvas>
+			<Section className='py-16 px-6 min-h-screen flex flex-col justify-center items-center gap-16 relative z-10'>
+				<motion.div layout transition={{ duration: 0.25 }}>
+					<motion.div
+						animate={{ opacity: 100 }}
+						initial={{ opacity: 0 }}
+						transition={{ duration: 0.5, delay: 1.25 }}
+					>
+						<Logo />
+					</motion.div>
+				</motion.div>
+				<AnimatePresence>
+					{show && (
+						<motion.div
+							layout
+							animate={{ opacity: 100, y: 0 }}
+							initial={{ opacity: 0, y: 100 }}
+							className='flex flex-col gap-10 max-w-[300px]'
+						>
+							<div className='flex flex-col gap-4 text-center'>
+								<SectionTitle>
+									Master the Cosmos with Galactic Shogun
+								</SectionTitle>
+								<p className='text-sm'>
+									Sign up now to receive exclusive updates and be the first to
+									conquer the cosmos!
+								</p>
+							</div>
+							<div className='flex flex-col gap-2'>
+								<Input placeholder='Enter your email' />
+								<Button>SUBSCRIBE</Button>
+							</div>
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</Section>
+		</>
 	);
 };
 
--- src/elements/Logo.js
+++ src/elements/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = ({ className }) => {
 	return (
 		<h1
-			className={`text-4xl font-bold leading-none text-center drop-shadow-[--logo-shadow] ${className}`}
+			className={`text-4xl md:text-6xl font-bold leading-none text-center drop-shadow-[--logo-shadow] ${className}`}
 		>
 			GALACTIC<div>SHOGUN</div>
 		</h1>
