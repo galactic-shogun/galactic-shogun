c171faf251994bfab902023b1c1b0566784d77d2
iamnotrodger <rodger.retanal24@gmail.com>
2023-06-27 19:52:01 -0400

    [feature] Feature Icon and Social Icons

---
+++ src/icons/Icon.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import NinjaIcon from './NinjaIcon';
+import SpaceIcon from './SpaceIcon';
+import SamuraiIcon from './SamuraiIcon';
+import SakuraIcon from './SakuraIcon';
+import InstagramIcon from './InstagramIcon';
+import FacebookIcon from './FacebookIcon';
+import TikTokIcon from './TikTokIcon';
+
+const Icon = ({ name, ...props }) => {
+	switch (name) {
+		case 'ninja':
+			return <NinjaIcon {...props} />;
+		case 'samurai':
+			return <SamuraiIcon {...props} />;
+		case 'sakura':
+			return <SakuraIcon {...props} />;
+		case 'space-ship':
+			return <SpaceIcon {...props} />;
+		case 'instagram':
+			return <InstagramIcon {...props} />;
+		case 'facebook':
+			return <FacebookIcon {...props} />;
+		case 'tiktok':
+			return <TikTokIcon {...props} />;
+		default:
+			return <SpaceIcon {...props} />;
+	}
+};
+
+export default Icon;
