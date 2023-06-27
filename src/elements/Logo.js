a12db7aa9fc00cfdafc5fad8fea85b0bf431c4db
iamnotrodger <rodger.retanal24@gmail.com>
2023-06-26 21:23:32 -0400

    [feature] `Logo` styled component

---
+++ src/elements/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = ({ className }) => {
+	return (
+		<h1
+			className={`text-4xl font-bold leading-none text-center drop-shadow-[--logo-shadow] ${className}`}
+		>
+			GALACTIC<div>SHOGUN</div>
+		</h1>
+	);
+};
+
+export default Logo;
