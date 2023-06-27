5c8cc04bdac21cddd21a82789ed974f4d5481b55
iamnotrodger <rodger.retanal24@gmail.com>
2023-06-26 21:25:18 -0400

    [update] `App` sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Hero from './components/Hero';
+import Intro from './components/Intro';
+import Features from './components/Features';
 
 const App = () => {
-	return <>Hello World</>;
+	return (
+		<>
+			<main>
+				<Hero />
+				<Intro />
+				<Features />
+			</main>
+		</>
+	);
 };
 
 export default App;
