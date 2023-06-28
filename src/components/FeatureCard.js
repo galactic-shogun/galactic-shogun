8acd414892581fdfe46822f74c905d75ab8d0b42
iamnotrodger <rodger.retanal24@gmail.com>
2023-06-27 20:19:05 -0400

    [update] `Feature`

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import SpaceIcon from '../icons/SpaceIcon';
+import Icon from '../icons/Icon';
 
-const FeatureCard = ({ title, description }) => {
+const FeatureCard = ({ icon, title, description }) => {
 	return (
-		<div className='p-6 rounded-lg bg-zinc-100'>
-			<SpaceIcon className='h-12 w-12 mx-auto mb-6 text-coolGray-900' />
+		<div className='text-center'>
+			<Icon name={icon} className='h-24 mx-auto mb-10 text-zinc-100' />
 			<div className='flex flex-col gap-2'>
-				<h4 className='text-xl font-serif text-black font-semibold'>{title}</h4>
-				<p className='font-serif text-gray-500'>{description}</p>
+				<h4 className='text-xl font-semibold font-serif text-zinc-100semibold'>
+					{title}
+				</h4>
+				<p className='font-serif text-zinc-100'>{description}</p>
 			</div>
 		</div>
 	);
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import Section from '../elements/Section';
-import SectionTitle from '../elements/SectionTitle';
+import MetalIcon from '../icons/MetalIcon';
 import FeatureCard from './FeatureCard';
 
 const Features = () => {
 	return (
-		<Section className='bg-gray-950 bg-gradient-to-b from-background to-10%'>
+		<section className='section relative overflow-hidden'>
 			<div className='flex flex-col gap-9'>
 				<div className='flex flex-col gap-4 text-center'>
-					<SectionTitle>When Strategy Meets Interstellar Warfare</SectionTitle>
+					<h2 className='section-title'>Social Deduction for Everyone</h2>
 					<p className='text-sm font-serif text-tertiary-light'>
 						Where honor, power, and strategy intersect. Are you ready to conquer
 						the stars?
 					</p>
 				</div>
-				<div className='flex flex-col gap-6'>
+				<div className='flex flex-col gap-10 py-10 px-6 border-2 rounded-lg bg-tertiary/[0.15]'>
 					<FeatureCard
-						title='Immersive Futuristic Setting'
-						description='Every element of the game has been meticulously crafted to transport you to a world where power and destiny collide.'
+						icon='samurai'
+						title='Fast & furious.'
+						description='Games take 15-45 minutes and set up is easy. The game state changes drastically every turn when a card is played.'
 					/>
 					<FeatureCard
-						title='Immersive Futuristic Setting'
-						description='Every element of the game has been meticulously crafted to transport you to a world where power and destiny collide.'
+						icon='ninja'
+						title='Can’t lie? All good.'
+						description='No one “dies” and those who can’t lie are given multiple opportunities to stay in the game.'
 					/>
 					<FeatureCard
-						title='Immersive Futuristic Setting'
-						description='Every element of the game has been meticulously crafted to transport you to a world where power and destiny collide.'
+						icon='sakura'
+						title='It’s not over til it’s over.'
+						description='All Play Cards have the ability to help you win the game, but you will always need plans A-Z as the other players disrupt your gameplay.'
 					/>
 				</div>
 			</div>
-		</Section>
+			<MetalIcon className='stroke-white/50 absolute z-0 bottom-1/4 right-0 translate-x-1/3' />
+			<MetalIcon className='stroke-white/50 absolute z-0 bottom-0 left-0 -translate-x-1/2' />
+		</section>
 	);
 };
 
