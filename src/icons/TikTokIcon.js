d3e1c30f4fdb3a6caf4f3a16aed620144187ba0f
iamnotrodger <rodger.retanal24@gmail.com>
2023-06-28 09:41:40 -0400

    [update] Icons name conventions

--- src/icons/TikTokIcon.js
+++ src/icons/TikTokIcon.js
@@ -5,11 +5,11 @@ const TikTokIcon = (props) => {
 		<svg
 			xmlns='http://www.w3.org/2000/svg'
 			viewBox='0 0 2859 3333'
-			shape-rendering='geometricPrecision'
-			text-rendering='geometricPrecision'
-			image-rendering='optimizeQuality'
-			fill-rule='evenodd'
-			clip-rule='evenodd'
+			shapeRendering='geometricPrecision'
+			textRendering='geometricPrecision'
+			imageRendering='optimizeQuality'
+			fillRule='evenodd'
+			clipRule='evenodd'
 			fill='currentColor'
 			{...props}
 		>
