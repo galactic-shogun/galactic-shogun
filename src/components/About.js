e19dc8e939347454d66556344ab007d617b0a350
iamnotrodger <rodger.retanal24@gmail.com>
2023-06-27 20:19:19 -0400

    [update] `About`

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import Section from '../elements/Section';
-import SectionTitle from '../elements/SectionTitle';
+import BoxImage from '../images/Box.png';
+import Dango from '../images/Dango.png';
 
 const About = () => {
 	return (
-		<Section className='bg-gradient-to-b from-gray-950 to-10%'>
+		<section className='section bg-gray-950 bg-gradient-to-b from-background to-25%'>
 			<div className='text-center'>
-				<SectionTitle>What to expect</SectionTitle>
+				<h2 className='section-title'>Game Contents</h2>
 			</div>
-			<div className='p-6 mt-6 rounded-lg bg-zinc-100'>
-				<div className='flex flex-col gap-2'>
-					<h4 className='text-xl font-serif text-black font-semibold'>
-						What’s in the box?
-					</h4>
-					<p className='font-serif text-gray-500'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit id venenatis
-						pretium risus euismod dictum egestas orci netus.
-					</p>
+			<div className='p-6 mt-44 rounded-lg bg-zinc-100 relative'>
+				<img
+					src={BoxImage}
+					alt='box'
+					className='absolute -top-full left-1/2 -translate-y-3.5 -translate-x-1/2'
+				/>
+				<ul className='font-serif text-gray-500 text-center'>
+					<li>1x Rule Sheet</li>
+					<li>8x Role Cards (4 Unique)</li>
+					<li>32x Play Cards (10 Unique)</li>
+					<li>Tons of Fun</li>
+				</ul>
+			</div>
+			<div className='p-6 mt-8 flex flex-col justify-center gap-6'>
+				<h3 className='text-xl font-serif font-bold text-center'>
+					Presented by
+					<div>Dango Games Inc.</div>
+				</h3>
+				<div className='flex justify-center'>
+					<img src={Dango} alt='dango' className='w-44' />
 				</div>
 			</div>
-			<div className='p-6 mt-8 flex flex-col gap-2'>
-				<h3 className='font-serif font-bold'>Who’s Dango Games Inc.?</h3>
-				<p className='font-serif'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit id venenatis
-					pretium risus euismod dictum egestas orci netus feugiat ut.
-				</p>
-			</div>
-		</Section>
+		</section>
 	);
 };
 
