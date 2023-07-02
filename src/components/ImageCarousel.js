ae9acd1b7de7439693a4fd6b1d000fe903ac4f01
iamnotrodger <rodger.retanal24@gmail.com>
2023-07-02 10:31:36 -0400

    [update] `ImageCarousel` direction and styling

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -11,7 +11,9 @@ const ImageCarousel = ({ images = [], interval }) => {
   useEffect(() => {
     if (interval) {
       intervalIdRef.current = setInterval(() => {
-        setIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setIndex(
+          (prevIndex) => (prevIndex - 1 + images.length) % images.length
+        );
       }, interval);
 
       return () => {
@@ -21,29 +23,29 @@ const ImageCarousel = ({ images = [], interval }) => {
   }, [images, interval]);
 
   useEffect(() => {
-    const length = images.length;
-    const shift = Math.floor(length / 2) - index;
-    const shiftedArray = images.slice(shift).concat(images.slice(0, shift));
-    setImagesOrder(shiftedArray);
-  }, [index, images]);
-
-  useEffect(() => {
     if (buttonClick && interval) {
       clearInterval(intervalIdRef.current);
       intervalIdRef.current = setInterval(() => {
-        setIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setIndex(
+          (prevIndex) => (prevIndex - 1 + images.length) % images.length
+        );
       }, interval);
       setButtonClick(false);
     }
   }, [buttonClick, interval, images]);
 
+  useEffect(() => {
+    const shift = index % images.length;
+    setImagesOrder([...images.slice(-shift), ...images.slice(0, -shift)]);
+  }, [index, images]);
+
   const handleNext = () => {
-    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
     setButtonClick(true);
   };
 
   const handlePrev = () => {
-    setIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
     setButtonClick(true);
   };
 
@@ -60,7 +62,7 @@ const ImageCarousel = ({ images = [], interval }) => {
 
   return (
     <MotionConfig transition={{ duration: 0.75, type: 'spring' }}>
-      <div className='relative mx-auto'>
+      <div className='relative'>
         <div className='relative flex h-full justify-center gap-4'>
           <AnimatePresence>
             {imagesOrder.map((image, i) => (
@@ -73,20 +75,20 @@ const ImageCarousel = ({ images = [], interval }) => {
                 layout
                 src={image}
                 alt='carousel'
-                className='aspect-[2/3] max-w-xs flex-1 rounded-xl object-cover'
+                className='aspect-[2/3] h-96 max-w-xs flex-1 rounded-lg object-cover'
               />
             ))}
           </AnimatePresence>
         </div>
 
         <button
-          className='absolute left-2 top-1/2 z-40 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md'
+          className='absolute left-2 top-1/2 z-40 flex h-8 w-8 -translate-y-1/2 transform items-center justify-center rounded-full bg-white shadow-md md:left-20'
           onClick={handlePrev}
         >
           <Icon name='arrow-left' className='h-6 w-6 text-black' />
         </button>
         <button
-          className='absolute right-2 top-1/2 z-40 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md'
+          className='absolute right-2 top-1/2 z-40 flex h-8 w-8 -translate-y-1/2 transform items-center justify-center rounded-full bg-white shadow-md md:right-20'
           onClick={handleNext}
         >
           <Icon name='arrow-right' className='h-6 w-6 text-black' />
