c409d5c5d4524ab34725b9b9475ca0052479f6af
iamnotrodger <rodger.retanal24@gmail.com>
2023-07-06 21:06:53 -0400

    [update] `Hero` section desktop style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import Logo from '../icons/Logo';
 import SpaceWarp from './SpaceWarp';
 import Icon from '../icons/Icon';
+import BoxCard from '../images/Box+Cards.png';
 
 const Hero = () => {
   const [show, setShow] = useState(false);
@@ -28,60 +29,73 @@ const Hero = () => {
       >
         {process.env.NODE_ENV === 'production' && <SpaceWarp />}
       </Canvas>
-      <section className='section relative z-0 flex min-h-screen flex-col items-center justify-center gap-20 bg-gradient-to-t from-background to-50%'>
-        <motion.div layout transition={{ duration: 0.25 }}>
-          <motion.div
-            animate={{ opacity: 100 }}
-            initial={{ opacity: 0 }}
-            transition={{ duration: 0.5, delay: 1 }}
-          >
-            <Logo className='w-64 md:w-96' />
+      <section className='section relative z-0 flex h-screen bg-gradient-to-t from-background to-50% '>
+        <div
+          className={`mx-auto flex h-full w-full max-w-screen-xl flex-col items-center justify-center gap-20 ${
+            show && 'md:items-start'
+          }`}
+        >
+          <motion.div layout transition={{ duration: 0.25 }}>
+            <motion.div
+              animate={{ opacity: 100 }}
+              initial={{ opacity: 0 }}
+              transition={{ duration: 0.5, delay: 1 }}
+            >
+              <Logo className={`w-64 ${show ? 'md:w-48' : 'md:w-96'}`} />
+            </motion.div>
           </motion.div>
-        </motion.div>
-        <AnimatePresence>
-          {show && (
-            <>
-              <motion.div
-                layout
-                animate={{ opacity: 100, y: 0 }}
-                initial={{ opacity: 0, y: 100 }}
-                className='mb-auto flex flex-col items-center gap-5 md:mb-0'
-              >
-                <div className='flex flex-col items-center gap-4 text-center'>
-                  <h3 className='font-serif text-sm font-semibold'>
-                    KICKSTARTER LAUNCH FALL 2023
-                  </h3>
-                  <h2 className='section-title text-primary md:text-4xl'>
-                    Master the Cosmos with
-                    <div>Galactic Shogun</div>
-                  </h2>
-                  <p className='font-serif text-sm'>
-                    A new fast-paced and dynamic social deduction board game for
-                    4-8 players!
-                  </p>
-                </div>
-                <div className='flex flex-col gap-2 md:max-w-xs'>
-                  <input className='input' placeholder='First Name' />
-                  <input className='input' placeholder='Email Address' />
-                  <button className='button transform hover:scale-105'>
-                    GET NOTIFIED
-                  </button>
-                </div>
-              </motion.div>
-              <motion.div
-                animate={{ opacity: 100 }}
-                initial={{ opacity: 0 }}
-                transition={{ duration: 0.5, delay: 0.75 }}
-                className='relative flex'
-              >
-                <Icon
-                  name='arrow-down'
-                  className='relative h-12 animate-bounce text-zinc-100'
-                />
-              </motion.div>
-            </>
-          )}
-        </AnimatePresence>
+          <AnimatePresence>
+            {show && (
+              <>
+                <motion.div
+                  layout
+                  animate={{ opacity: 100, y: 0 }}
+                  initial={{ opacity: 0, y: 100 }}
+                  className='mb-auto flex w-full items-center justify-between'
+                >
+                  <div className='flex flex-col items-center gap-5 md:items-start'>
+                    <div className='flex flex-col items-center gap-4 text-center md:items-start md:text-left'>
+                      <h3 className='font-serif text-sm font-semibold'>
+                        KICKSTARTER LAUNCH FALL 2023
+                      </h3>
+                      <h2 className='section-title text-primary md:text-5xl'>
+                        Master the Cosmos with
+                        <div>Galactic Shogun</div>
+                      </h2>
+                      <p className='font-serif text-sm'>
+                        A new fast-paced and dynamic social deduction board game
+                        for 4-8 players!
+                      </p>
+                    </div>
+                    <div className='flex flex-col gap-2 md:w-full md:max-w-md'>
+                      <input className='input' placeholder='First Name' />
+                      <input className='input' placeholder='Email Address' />
+                      <button className='button transform hover:scale-105'>
+                        GET NOTIFIED
+                      </button>
+                    </div>
+                  </div>
+                  <img
+                    src={BoxCard}
+                    alt='box+card'
+                    className='hidden h-auto w-[500px] object-contain md:block'
+                  />
+                </motion.div>
+                <motion.div
+                  animate={{ opacity: 100 }}
+                  initial={{ opacity: 0 }}
+                  transition={{ duration: 0.5, delay: 0.75 }}
+                  className='absolute bottom-[10%] left-1/2 flex -translate-x-1/2'
+                >
+                  <Icon
+                    name='arrow-down'
+                    className='relative h-12 animate-bounce text-zinc-100'
+                  />
+                </motion.div>
+              </>
+            )}
+          </AnimatePresence>
+        </div>
       </section>
     </>
   );
